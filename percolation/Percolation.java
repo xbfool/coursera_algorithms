87ad3b62bb11c01c62b5a0f74cbd99c17614a4a9
xbfool <xbfool@gmail.com>
2020-11-21 22:52:59 +0800

    project 1 ok

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,42 +1,107 @@
-/* *****************************************************************************
- *  Name:              Alan Turing
- *  Coursera User ID:  123456
- *  Last modified:     1/1/2019
- **************************************************************************** */
+/******************************************************************************
+ *  Compilation:  javac Percolation.java
+ *  Execution:    java Percolation n
+ *  This program takes the grid size n as a command-line argument.
+ *  Then, the user repeatedly clicks sites to open with the mouse.
+ *  After each site is opened, it draws full sites in light blue,
+ *  open sites (that aren't full) in white, and blocked sites in black.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private enum SiteState {
+        eClose,
+        eOpen,
+        eFull
+    }
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-
+        size = n;
+        data = new SiteState[n * n + 2];
+        for (int i = 0; i < n * n + 2; i++) {
+            data[i] = SiteState.eClose;
+        }
+        numberOfOpen = 0;
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        start = n * n;
+        end = n * n + 1;
+        data[start] = SiteState.eFull;
+        data[end] = SiteState.eOpen;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            System.out.println("row: " + row + " col: " + col);
+            throw new IllegalArgumentException();
+        }
+        int index = (row - 1) * size + col - 1;
+        if (data[index] == SiteState.eClose) {
+            data[index] = SiteState.eOpen;
+            numberOfOpen++;
+            if (row == 1) {
+                uf.union(index, start);
+            }
+            if (row == size) {
+                uf.union(index, end);
+            }
+            if (row > 1 && isOpen(row - 1, col)) {
+                uf.union(index, index - size);
+            }
+            if (row < size && isOpen(row + 1, col)) {
+                uf.union(index, index + size);
+            }
+            if (col > 1 && isOpen(row, col - 1)) {
+                uf.union(index, index - 1);
+            }
+            if (col < size && isOpen(row, col + 1)) {
+                uf.union(index, index + 1);
+            }
 
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            System.out.println("row: " + row + " col: " + col);
+            throw new IllegalArgumentException();
+        }
+        return data[(row - 1) * size + col - 1] != SiteState.eClose;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            System.out.println("row: " + row + " col: " + col);
+            throw new IllegalArgumentException();
+        }
+        return data[(row - 1) * size + col - 1] == SiteState.eFull;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return numberOfOpen;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return false;
+        return uf.connected(start, end);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        
+
     }
+
+    private SiteState[] data;
+    private int numberOfOpen;
+    private int size;
+    private int start;
+    private int end;
+    private WeightedQuickUnionUF uf;
+
 }
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,85 @@
+/******************************************************************************
+ *  Compilation:  javac PercolationStats.java
+ *  Execution:    java PercolationStats n
+ *  Dependencies: Percolation.java
+ *  This program takes the grid size n as a command-line argument.
+ *  Then, the user repeatedly clicks sites to open with the mouse.
+ *  After each site is opened, it draws full sites in light blue,
+ *  open sites (that aren't full) in white, and blocked sites in black.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private double[] trailResults;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        trailResults = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Integer[] intArray = new Integer[n * n];
+
+            for (int j = 0; j < n * n; j++) {
+                intArray[j] = j;
+            }
+            StdRandom.shuffle(intArray);
+            Percolation p = new Percolation(n);
+            for (int k = 0; i < n * n && !p.percolates(); k++) {
+                int col = intArray[k] % n + 1;
+                int row = intArray[k] / n + 1;
+                p.open(col, row);
+                if (p.percolates()) {
+                    trailResults[i] = (double) k / ((double) n * n);
+                }
+            }
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(trailResults);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(trailResults);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        double m = mean();
+        double s = stddev();
+        double r = m - 1.96 * s / trailResults.length;
+        return r;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        double m = mean();
+        double s = stddev();
+        double r = m + 1.96 * s / trailResults.length;
+        return r;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trails = Integer.parseInt(args[1]);
+
+        PercolationStats s = new PercolationStats(n, trails);
+        System.out.println("mean                    = " + s.mean());
+        System.out.println("stddev                  = " + s.stddev());
+        System.out
+                .println("95% confidence interval = [" + s.confidenceLo() + ", " + s.confidenceHi()
+                                 + "]");
+
+
+    }
+
+
+}

376959170e5e75855469e68ca31caa49d61396ed
xbfool <xbfool@gmail.com>
2021-04-07 00:43:53 +0800

    add more test

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -12,23 +12,24 @@ import java.util.ArrayList;
 public class Board {
     private int[][] tiles;
     private int size;
+
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles){
+    public Board(int[][] tiles) {
         size = tiles.length;
         this.tiles = new int[size][size];
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 this.tiles[i][j] = tiles[i][j];
             }
         }
     }
 
     // string representation of this board
-    public String toString(){
+    public String toString() {
         String s1 = String.format("%d\n", size);
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 s1 += String.format("%d ", tiles[i][j]);
             }
             s1 += "\n";
@@ -37,16 +38,16 @@ public class Board {
     }
 
     // board dimension n
-    public int dimension(){
+    public int dimension() {
         return size;
     }
 
     // number of tiles out of place
-    public int hamming(){
+    public int hamming() {
         int x = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if(tiles[i][j] != 0 && tiles[i][j] != i * size + j + 1){
+                if (tiles[i][j] != 0 && tiles[i][j] != i * size + j + 1) {
                     x++;
                 }
             }
@@ -55,12 +56,12 @@ public class Board {
     }
 
     // sum of Manhattan distances between tiles and goal
-    public int manhattan(){
+    public int manhattan() {
         int x = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 int tmp = tiles[i][j];
-                if(tmp == 0)
+                if (tmp == 0)
                     continue;
                 int tmi = (tmp - 1) / size;
                 int tmj = (tmp - 1) % size;
@@ -71,19 +72,20 @@ public class Board {
     }
 
     // is this board the goal board?
-    public boolean isGoal(){
+    public boolean isGoal() {
         return hamming() == 0;
     }
 
     // does this board equal y?
-    public boolean equals(Object y){
+    public boolean equals(Object y) {
         Board yy = (Board) y;
-        if(yy.size != size){
+        if (yy.size != size) {
             return false;
-        }else{
+        }
+        else {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    if (tiles[i][j] != yy.tiles[i][j]){
+                    if (tiles[i][j] != yy.tiles[i][j]) {
                         return false;
                     }
                 }
@@ -93,53 +95,53 @@ public class Board {
     }
 
     // all neighboring boards
-    public Iterable<Board> neighbors(){
+    public Iterable<Board> neighbors() {
         ArrayList<Board> ret = new ArrayList<Board>();
         int x = 0;
         int y = 0;
         Boolean found = false;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if(tiles[i][j] == 0){
+                if (tiles[i][j] == 0) {
                     y = i;
                     x = j;
                     break;
                 }
             }
-            if(found){
+            if (found) {
                 break;
             }
         }
         //up
-        if(y > 0){
+        if (y > 0) {
             Board b = new Board(this.tiles);
-            b.tiles[y-1][x] = 0;
-            b.tiles[y][x] = tiles[y-1][x];
+            b.tiles[y - 1][x] = 0;
+            b.tiles[y][x] = tiles[y - 1][x];
             ret.add(b);
         }
-        if(y < size - 1){
+        if (y < size - 1) {
             Board b = new Board(this.tiles);
-            b.tiles[y+1][x] = 0;
-            b.tiles[y][x] = tiles[y+1][x];
+            b.tiles[y + 1][x] = 0;
+            b.tiles[y][x] = tiles[y + 1][x];
             ret.add(b);
         }
-        if(x > 0){
+        if (x > 0) {
             Board b = new Board(this.tiles);
-            b.tiles[y][x-1] = 0;
-            b.tiles[y][x] = tiles[y][x-1];
+            b.tiles[y][x - 1] = 0;
+            b.tiles[y][x] = tiles[y][x - 1];
             ret.add(b);
         }
-        if(x < size - 1){
+        if (x < size - 1) {
             Board b = new Board(this.tiles);
-            b.tiles[y][x+1] = 0;
-            b.tiles[y][x] = tiles[y][x+1];
+            b.tiles[y][x + 1] = 0;
+            b.tiles[y][x] = tiles[y][x + 1];
             ret.add(b);
         }
         return ret;
     }
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin(){
+    public Board twin() {
         int x1 = 0;
         int y1 = 0;
         int x2 = 0;
@@ -148,19 +150,20 @@ public class Board {
         Board b = new Board(tiles);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if(tiles[i][j] != 0){
-                    if(found == 0){
+                if (tiles[i][j] != 0) {
+                    if (found == 0) {
                         x1 = j;
                         y1 = i;
                         found++;
-                    }else{
+                    }
+                    else {
                         x2 = j;
                         y2 = j;
                         found++;
                     }
                 }
             }
-            if(found >= 2){
+            if (found >= 2) {
                 break;
             }
         }
@@ -170,7 +173,7 @@ public class Board {
     }
 
     // unit testing (not graded)
-    public static void main(String[] args){
+    public static void main(String[] args) {
         for (String filename : args) {
 
             // read in the board specified in the filename
@@ -187,6 +190,10 @@ public class Board {
             Board initial = new Board(tiles);
             //Solver solver = new Solver(initial);
             StdOut.println(initial.toString());
+            StdOut.println(initial.dimension());
+            StdOut.println(initial.hamming());
+            StdOut.println(initial.manhattan());
+            StdOut.println(initial.isGoal());
             //StdOut.println(filename + ": " + solver.moves());
         }
     }
